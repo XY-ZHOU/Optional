48477b723ed343d8bb2ff2a1e0775ddb09dc74ef
XY-ZHOU <731986251@qq.com>
2019-03-10 21:55:22 +0800

    add function of using judge function

--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -10,4 +10,19 @@ public class Main {
         boolean isPriceSuitable = opt.filter(n -> n.getPrice() >= 1000 && n.getPrice() <= 2000).isPresent();
         return isPhone && isPriceSuitable;
     }
+    public static void main(String[] args) {
+        Product productOne = new Product(1, "手机", 1500);
+        Product productTwo = new Product(2, "手机", 900);
+        Product productThree = new Product(3, "平板", 1700);
+
+        Optional<Product> optOne = Optional.ofNullable(productOne);
+        Optional<Product> optTwo = Optional.ofNullable(productTwo);
+        Optional<Product> optThree = Optional.ofNullable(productThree);
+        Optional<Product> optFour = Optional.ofNullable(null);
+        System.out.println(judge(optOne));
+        System.out.println(judge(optTwo));
+        System.out.println(judge(optThree));
+        System.out.println(judge(optFour));
+
+    }
 }
